82ba268de19d4c99913441bb78be3ee7010a6c69
Sebastián Brito <sebastianbrito68@gmail.com>
2023-09-15 17:44:56 -0300

    initial commit

---
+++ Proyecto Noticia/js/app.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded",function () {  
+    document.getElementById("seccComent").addEventListener("submit",function(evento)
+    {
+        evento.preventDefault();
+    })
+})
