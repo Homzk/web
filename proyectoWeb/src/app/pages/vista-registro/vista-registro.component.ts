44721af2c497a29b592ecf50edad2bf2207ba429
Álvaro Flores Rocha <f.alvaro.ro@gmail.com>
2023-10-28 16:33:32 -0300

    comunas-region

--- proyectoWeb/src/app/pages/vista-registro/vista-registro.component.ts
+++ proyectoWeb/src/app/pages/vista-registro/vista-registro.component.ts
@@ -1,18 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { RegionesService } from 'src/app/services/regiones.service';
+import { ComunasService } from 'src/app/services/comunas.service';
 
 @Component({
   selector: 'app-vista-registro',
   templateUrl: './vista-registro.component.html',
-  styleUrls: ['./vista-Registro.component.scss']
+  styleUrls: ['./vista-registro.component.scss']
 })
 export class VistaRegistroComponent implements OnInit {
   regiones: any[] = [];
-  constructor(private RegionesService: RegionesService) { }
+  comunas: string[] = [];
+  selectedRegion: any;
+
+  constructor(private regionesService: RegionesService, private comunasService: ComunasService) {}
+
   ngOnInit(): void {
-    this.RegionesService.getRegiones().subscribe((data) =>  {
+    this.regionesService.getRegiones().subscribe((data) => {
       this.regiones = data;
-    })
+    });
+  }
+
+  onRegionChange(event: Event) {
+    const selectedRegionId = +(event.target as HTMLSelectElement).value;
+    this.comunasService.getComunas().subscribe((comunas) => {
+      this.comunas = comunas[selectedRegionId] || [];
+    });
   }
 }
+
 
